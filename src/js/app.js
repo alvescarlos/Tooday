eabaf0933e54b6533ab841e698b4d7019454a670
alvescarlos <alves.carlos@gmail.com>
2017-09-14 11:29:50 +0100

    Total number os tasks

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,9 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var i;
+var nrtasks = 0;
 var tasksarray = [];
-var menuitem = 0;
+var menuitem = 1;
 
 var today = new Date();
 var todaydate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -26,7 +27,7 @@ var menu = new UI.Menu({
   textColor: 'black',
   highlightBackgroundColor: 'black',
   highlightTextColor: 'white',
-  sections: [{ title: 'Tasks Today' }]
+  sections: [{ title: 'Tooday' }]
 });
 
 function getTasks() {
@@ -36,16 +37,19 @@ function getTasks() {
       console.log('Today: ' + todaydate + ' (' + todayunixtime + ')');
       console.log('Tomorrow: ' + tomorrowdate + ' (' + tomorrowunixtime + ')');
       
-      menu.item(0,0,{title: ""});
+      //menu.item(0,0,{title: ""});
                      
       for (i = 1; i < data.length; i++) {
         //console.log('Task: ' + data[i].title + ' Due Date:' + data[i].duedate);
         if(data[i].duedate > todayunixtime && data[i].duedate < tomorrowunixtime && data[i].folder == "2895059"){
           console.log("found task " + data[i].title);
-          menu.item(0,menuitem+1,{title: data[i].title});
+          menu.item(0,menuitem,{title: data[i].title});
           menuitem++;
         }
       }
+      
+      //Total number os tasks
+      menu.item(0,0,{title: "Tasks: " + (menuitem-1)});
     },
     function(error) {
       console.log('Error receiving Toodledo data.');  
@@ -59,16 +63,20 @@ function getTasks() {
       console.log('Today: ' + todaydate + ' (' + todayunixtime + ')');
       console.log('Tomorrow: ' + tomorrowdate + ' (' + tomorrowunixtime + ')');
       
-      menu.item(0,0,{title: ""});
+      //menu.item(0,0,{title: ""});
                      
       for (i = 1; i < data.length; i++) {
         //console.log('Task: ' + data[i].title + ' Due Date:' + data[i].duedate);
         if(data[i].duedate > todayunixtime && data[i].duedate < tomorrowunixtime && data[i].folder == "2895059"){
           console.log("found task " + data[i].title);
-          menu.item(0,menuitem+1,{title: data[i].title});
+          menu.item(0,menuitem,{title: data[i].title});
           menuitem++;
         }
       }
+      
+      //Total number os tasks
+      menu.item(0,0,{title: "Tasks: " + (menuitem-1)});
+
     },
     function(error) {
       console.log('Error receiving Toodledo data.');  
